5fab043c786f6a76f43cd9625e264b08d29bb9ff
Daniiel314 <36395002+Daniiel314@users.noreply.github.com>
2025-09-18 05:36:49 -0600

    Implementacion de la práctica 4

--- src/main/java/com/product/api/service/SvcCategoryImp.java
+++ src/main/java/com/product/api/service/SvcCategoryImp.java
@@ -2,8 +2,11 @@ package com.product.api.service;
 
 import com.product.api.entity.Category;
 import com.product.api.repository.RepoCategory;
+import com.product.exception.ApiException;
+import com.product.exception.DBAccessException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,11 @@ public class SvcCategoryImp implements SvcCategory {
 	
     @Override
     public ResponseEntity<List<Category>> getCategories() {
-        return new ResponseEntity<>(repo.getCategories(), HttpStatus.OK); 
+    	try {
+            return new ResponseEntity<>(repo.getCategories(), HttpStatus.OK); 
+        } catch (DataAccessException e) {
+            // Lanzamos la excepción personalizada
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
 }
