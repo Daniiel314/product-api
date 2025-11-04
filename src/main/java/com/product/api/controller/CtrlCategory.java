329e5fe4492cd02144b1dd45596a999b8b59fbce
Daniiel314 <36395002+Daniiel314@users.noreply.github.com>
2025-11-03 20:35:21 -0600

    Implementacion de la practica 8

--- src/main/java/com/product/api/controller/CtrlCategory.java
+++ src/main/java/com/product/api/controller/CtrlCategory.java
@@ -5,6 +5,8 @@ import com.product.api.dto.in.DtoCategoryIn;
 import com.product.api.entity.Category;
 import com.product.api.service.SvcCategory;
 
+import io.swagger.v3.oas.annotations.Operation; // Importación añadida
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,37 +21,55 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Category", description = "Catálogo de categorías")
 @RestController
 public class CtrlCategory {
 
     @Autowired
     private SvcCategory svc;
-
+    
+    
+    @Operation(summary = "Listar todas las categorías", 
+    		description = "Devuelve una lista de todas las categorías,"
+    				+ " tanto activas como inactivas.")
     @GetMapping("/category")
     public ResponseEntity<List<Category>> findAll() {
     	return ResponseEntity.ok(svc.findAll());
     }
     
+    @Operation(summary = "Listar categorías activas", 
+    		description = "Devuelve una lista de solo las categorías"
+    				+ " que están marcadas como activas.")
     @GetMapping("/category/active")
     public ResponseEntity<List<Category>> findActive(){
     	return ResponseEntity.ok(svc.findActive());
     }
     
+    @Operation(summary = "Crear categoría", 
+    		description = "Registra una nueva categoría en el sistema.")
     @PostMapping("/category")
     public ResponseEntity<ApiResponse> create(@Valid @RequestBody DtoCategoryIn in) {
         return ResponseEntity.ok(svc.create(in));
     }
 
+    @Operation(summary = "Actualizar categoría", 
+    		description = "Actualiza los datos de una categoría existente, "
+    				+ "identificada por su ID.")
     @PutMapping("/category/{id}")
-    public ResponseEntity<ApiResponse> update(@PathVariable Integer id, @Valid @RequestBody DtoCategoryIn in) {
+    public ResponseEntity<ApiResponse> update(@PathVariable Integer id, 
+    		@Valid @RequestBody DtoCategoryIn in) {
         return ResponseEntity.ok(svc.update(in, id));
     }
     
+    @Operation(summary = "Habilitar categoría", 
+    		description = "Marca una categoría como activa usando su ID.")
     @PatchMapping("/category/{id}/enable")
     public ResponseEntity<ApiResponse> enable(@PathVariable Integer id) {
         return ResponseEntity.ok(svc.enable(id));
     }
     
+    @Operation(summary = "Deshabilitar categoría", 
+    		description = "Marca una categoría como inactiva usando su ID.")
     @PatchMapping("/category/{id}/disable")
     public ResponseEntity<ApiResponse> disable(@PathVariable Integer id) {
         return ResponseEntity.ok(svc.disable(id));
