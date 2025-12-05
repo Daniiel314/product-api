5613b36ecfae97ce5b711cca8b3f77c08af35884
Daniiel314 <36395002+Daniiel314@users.noreply.github.com>
2025-12-05 02:49:50 -0600

    agregar funcionalidad de descubrimiento

--- src/main/java/com/product/ProductApplication.java
+++ src/main/java/com/product/ProductApplication.java
@@ -2,12 +2,13 @@ package com.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient; // Importar esto
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
 	}
-
 }
